ec425cc098955fbbf29fceb07c71a129be9dfed0
Obed Rayo <rayovianamiltonobed@gmail.com>
2022-08-24 20:09:50 -0500

    Añadiendo base del proyecto y clase Empresa

---
+++ src/main/java/com/udea/proyet/ProyetApplication.java
@@ -0,0 +1,13 @@
+package com.udea.proyet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/proyet/ProyetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.proyet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
