2b3f166ec7cc454324e5de7808bd4f320b25e1d6
Obed Rayo <rayovianamiltonobed@gmail.com>
2022-09-06 17:47:38 -0500

    Cambie la ruta del post de /enterpise a /enterprises como se muestra en el documento

--- src/main/java/com/udea/proyect/Controllers/ControllerEnterprise.java
+++ src/main/java/com/udea/proyect/Controllers/ControllerEnterprise.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,7 +38,7 @@ public class ControllerEnterprise {
         }
     }
 
-    @PostMapping("/enterprise")
+    @PostMapping("/enterprises")
     public ResponseEntity<Enterprise> saveEnterprise (@RequestBody Enterprise enterprise){
         try{
             enterpriseService.createEnterprise(enterprise);
