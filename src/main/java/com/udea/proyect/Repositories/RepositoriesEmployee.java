370e5b4665a6fdd0adebed26e0d404eb49ea5858
Obed Rayo <rayovianamiltonobed@gmail.com>
2022-09-07 21:02:26 -0500

    Elimine el deleteEmployee

--- src/main/java/com/udea/proyect/Repositories/RepositoriesEmployee.java
+++ src/main/java/com/udea/proyect/Repositories/RepositoriesEmployee.java
@@ -1,18 +1,8 @@
 package com.udea.proyect.Repositories;
 
-
-
-import java.time.LocalDate;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
 import com.udea.proyect.Entities.*;
-
 
 public interface RepositoriesEmployee extends JpaRepository<Employee,Integer>{
 
-
-    
 }
--- src/main/java/com/udea/proyect/Repositories/RepositoriesEnterpise.java
+++ src/main/java/com/udea/proyect/Repositories/RepositoriesEnterpise.java
@@ -1,11 +1,6 @@
 package com.udea.proyect.Repositories;
 
-import java.time.LocalDate;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
 import com.udea.proyect.Entities.Enterprise;
 
 public interface RepositoriesEnterpise extends JpaRepository<Enterprise,Integer>{
