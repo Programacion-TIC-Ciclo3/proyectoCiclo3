557017e40c0f662d0621c6f18322032da2074379
jpiedrahita26 <julianlcontrerasp@gmail.com>
2022-09-10 12:18:17 -0500

    todo corregido creado controlador, servicio y repositorio

--- src/main/java/com/udea/proyect/Services/TransactionServices.java
+++ src/main/java/com/udea/proyect/Services/TransactionServices.java
@@ -1,5 +1,35 @@
 package com.udea.proyect.Services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udea.proyect.Entities.Transaction;
+import com.udea.proyect.Repositories.RepositoriesTransaction;
+
+@Service
 public class TransactionServices {
+
+    @Autowired
+    private RepositoriesTransaction repositoryTransaction;
+
+    public List<Transaction> getTransaction(){
+        return repositoryTransaction.findAll();
+    }
+
+    public String createTransaction(Transaction transaction){
+        repositoryTransaction.save(transaction);
+        return "Transaction creada exitosamente";
+    }
+
+    public Transaction getTransactionById(Integer id){
+        return repositoryTransaction.findById(id).get();
+    }
+
+    public String deleteTransactionById(Integer id){
+        repositoryTransaction.deleteById(id);
+        return "Transacion eliminado correctamente";
+    }
     
 }
